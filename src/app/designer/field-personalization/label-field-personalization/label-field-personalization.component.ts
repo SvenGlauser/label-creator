6801421389825db555925e876e02f050bf525a44
Sven Glauser <sven.glauser@divtec.ch>
2024-04-04 16:12:38 +0200

    Font style

--- src/app/designer/field-personalization/label-field-personalization/label-field-personalization.component.ts
+++ src/app/designer/field-personalization/label-field-personalization/label-field-personalization.component.ts
@@ -39,6 +39,8 @@ export class LabelFieldPersonalizationComponent implements OnInit, FieldPersonal
   @Input()
   public form?: AbstractControl;
 
+  protected stylesSelection?: string[];
+
   constructor(private fontService: FontService) {}
 
   public hasChanged(field: CommonField, oldField: CommonField): boolean {
@@ -47,6 +49,9 @@ export class LabelFieldPersonalizationComponent implements OnInit, FieldPersonal
       (<LabelField>field).fontFamily == (<LabelField>oldField).fontFamily &&
       (<LabelField>field).fontSize == (<LabelField>oldField).fontSize &&
       (<LabelField>field).textAlign == (<LabelField>oldField).textAlign &&
+      (<LabelField>field).bold == (<LabelField>oldField).bold &&
+      (<LabelField>field).italic == (<LabelField>oldField).italic &&
+      (<LabelField>field).underline == (<LabelField>oldField).underline &&
       (<LabelField>field).verticalTextAlign == (<LabelField>oldField).verticalTextAlign &&
       (<LabelField>field).color == (<LabelField>oldField).color &&
       (<LabelField>field).backgroundColor == (<LabelField>oldField).backgroundColor
@@ -59,6 +64,9 @@ export class LabelFieldPersonalizationComponent implements OnInit, FieldPersonal
     newField.fontSize = Number.parseInt(this.form!.get('fontSize')!.value);
     newField.fontFamily = this.form!.get('fontFamily')!.value;
     newField.textAlign = this.form!.get('textAlign')!.value;
+    newField.bold = this.form!.get('bold')!.value;
+    newField.italic = this.form!.get('italic')!.value;
+    newField.underline = this.form!.get('underline')!.value;
     newField.verticalTextAlign = this.form!.get('verticalTextAlign')!.value;
     newField.color = this.form!.get('color')!.value;
     newField.backgroundColor = this.form!.get('backgroundColor')!.value;
@@ -70,9 +78,13 @@ export class LabelFieldPersonalizationComponent implements OnInit, FieldPersonal
     this.form!.get('fontSize')!.setValue((<LabelField>field).fontSize, { emitEvent: false })
     this.form!.get('fontFamily')!.setValue((<LabelField>field).fontFamily, { emitEvent: false })
     this.form!.get('textAlign')!.setValue((<LabelField>field).textAlign, { emitEvent: false })
+    this.form!.get('bold')!.setValue((<LabelField>field).bold, { emitEvent: false })
+    this.form!.get('italic')!.setValue((<LabelField>field).italic, { emitEvent: false })
+    this.form!.get('underline')!.setValue((<LabelField>field).underline, { emitEvent: false })
     this.form!.get('verticalTextAlign')!.setValue((<LabelField>field).verticalTextAlign, { emitEvent: false })
     this.form!.get('color')!.setValue((<LabelField>field).color, { emitEvent: false })
     this.form!.get('backgroundColor')!.setValue((<LabelField>field).backgroundColor, { emitEvent: false })
+    this.initStyleValues();
   }
 
   /**
@@ -83,6 +95,9 @@ export class LabelFieldPersonalizationComponent implements OnInit, FieldPersonal
     (<FormGroup>this.form).addControl('fontSize', new FormControl(), { emitEvent: false });
     (<FormGroup>this.form).addControl('fontFamily', new FormControl(), { emitEvent: false });
     (<FormGroup>this.form).addControl('textAlign', new FormControl(), { emitEvent: false });
+    (<FormGroup>this.form).addControl('bold', new FormControl(), { emitEvent: false });
+    (<FormGroup>this.form).addControl('italic', new FormControl(), { emitEvent: false });
+    (<FormGroup>this.form).addControl('underline', new FormControl(), { emitEvent: false });
     (<FormGroup>this.form).addControl('verticalTextAlign', new FormControl(), { emitEvent: false });
     (<FormGroup>this.form).addControl('color', new FormControl(), { emitEvent: false });
     (<FormGroup>this.form).addControl('backgroundColor', new FormControl(), { emitEvent: false });
@@ -101,5 +116,54 @@ export class LabelFieldPersonalizationComponent implements OnInit, FieldPersonal
    */
   protected getFonts(): string[] {
     return this.fontService.getFonts();
+  }
+
+  /**
+   * Initialise les valeurs sélectionnées pour les styles
+   */
+  private initStyleValues(): void {
+    let selection: {[key: string]: boolean} = {
+      bold: !!this.form!.get('bold')!.value,
+      italic: !!this.form!.get('italic')!.value,
+      underline: !!this.form!.get('underline')!.value,
+    }
+
+    const arrayOfStyle: string[] = [];
+    for (const selectionKey in selection) {
+      if (selection[selectionKey]) {
+        arrayOfStyle.push(selectionKey);
+      }
+    }
+
+    this.stylesSelection = arrayOfStyle;
+  }
+
+  /**
+   * Met à jour les champs de style
+   * @param styles Nouveaux styles
+   */
+  protected setStyleValues(styles: string[]): void {
+    if (this.stylesSelection && styles != this.stylesSelection) {
+      this.stylesSelection.length = 0
+
+      if (styles.includes('bold')) {
+        this.stylesSelection.push('bold');
+        this.form!.get('bold')!.setValue(true, {emitEvent: true});
+      } else {
+        this.form!.get('bold')!.setValue(false, {emitEvent: true});
+      }
+      if (styles.includes('italic')) {
+        this.stylesSelection.push('italic');
+        this.form!.get('italic')!.setValue(true, {emitEvent: true});
+      } else {
+        this.form!.get('italic')!.setValue(false, {emitEvent: true});
+      }
+      if (styles.includes('underline')) {
+        this.stylesSelection.push('underline');
+        this.form!.get('underline')!.setValue(true, {emitEvent: true});
+      } else {
+        this.form!.get('underline')!.setValue(false, {emitEvent: true});
+      }
+    }
   }
 }
--- src/app/designer/fields/label-field/label-field.ts
+++ src/app/designer/fields/label-field/label-field.ts
@@ -7,6 +7,9 @@ export interface LabelField extends CommonField {
   content: string;
   fontSize: number;
   fontFamily: string;
+  bold: boolean;
+  italic: boolean;
+  underline: boolean;
   textAlign: 'center' | 'left' | 'right' | 'justify';
   verticalTextAlign: 'center' | 'flex-end' | 'flex-start';
   color: string;
